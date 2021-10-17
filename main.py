e9b34bbb3e99af013e60f10d050a06985752caa7
Mehmet Yilmaz <myilmaz8@web.de>
2021-10-17 14:48:01 +0200

    main file

---
+++ main.py
@@ -0,0 +1,6 @@
+def konto_erstellen():
+    
+
+antwort= input('Was kann ich fuer Sie tun?\n1. Konto erstellen.\n2. Geld abheben.\n3. Geld einzahlen.\n4. Geld ueberweisen.\n5. Geld investieren.\nWaehlen Sie bitte eine Nummer: ')
+if antwort=="1":
+    print(1)
